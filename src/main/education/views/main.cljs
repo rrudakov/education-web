abb63cca4475f70896f11b2fa8004d0a75f915fa
Roman Rudakov <rrudakov@protonmail.com>
2020-05-16 23:55:57 +0200

    Add basic wysiwyg editor

--- src/main/education/views/main.cljs
+++ src/main/education/views/main.cljs
@@ -1,12 +1,15 @@
 (ns education.views.main
   (:require ["@material-ui/core" :as mui :refer [createMuiTheme ThemeProvider]]
+            ["react-draft-wysiwyg" :refer [Editor]]
+            ["draft-js" :refer [EditorState convertFromRaw convertToRaw]]
             [education.subs.main :as subs]
             [education.views.error-message :refer [error-message]]
             [education.views.footer :refer [footer]]
             [education.views.header :refer [header]]
             [education.views.home.main :refer [home-component]]
             [education.views.signup.main :refer [signup-dialog]]
-            [re-frame.core :as rf]))
+            [re-frame.core :as rf]
+            [reagent.core :as r]))
 
 (def sections
   "Sections for main navigation."
@@ -46,6 +49,22 @@
     :article [:div]
     :not-found [:div]))
 
+(def editorState (r/atom (.createEmpty EditorState)))
+(def content (r/atom (.stringify js/JSON (convertToRaw (.getCurrentContent (.createEmpty EditorState))))))
+
+(defn readonly-editor
+  []
+  [:div
+   [:> Editor {:editorState (.createWithContent EditorState (convertFromRaw (.parse js/JSON @content)))
+               :toolbarHidden true
+               :readOnly true}]])
+
+(defn editor
+  []
+  [:> Editor {:editorState @editorState
+              :onChange #(reset! content (.stringify js/JSON (convertToRaw (.getCurrentContent @editorState))))
+              :onEditorStateChange #(reset! editorState %)}])
+
 (defn main-panel
   "Main application component."
   []
@@ -54,9 +73,11 @@
    [:> mui/Container {:maxWidth :lg}
     [error-message]
     [signup-dialog]
+    ;; [readonly-editor]
     [header {:title    "Blog"
              :sections sections}]
-    [panels @(rf/subscribe [::subs/active-panel])]]
+    [panels @(rf/subscribe [::subs/active-panel])]
+    [editor]]
    [footer
     {:description "Some description"
      :title       "Footer title"}]])
